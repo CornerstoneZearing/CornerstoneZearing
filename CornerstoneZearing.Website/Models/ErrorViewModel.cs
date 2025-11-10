4f0a220befa4bed5a89860f975819fa3cd90e99d
Ryan Heldt <ryan@ryanheldt.com>
2025-11-09 19:13:30 -0600

    First commit. Start of application.

---
+++ CornerstoneZearing.Website/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CornerstoneZearing.Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
