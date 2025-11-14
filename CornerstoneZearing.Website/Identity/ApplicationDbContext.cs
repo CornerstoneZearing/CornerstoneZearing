8dc6feb8510656ba36e3136b9fd9b2740b46caab
Ryan Heldt <ryan@ryanheldt.com>
2025-11-13 21:46:51 -0600

    Added ASP.NET identity., update to .NET 10.

---
+++ CornerstoneZearing.Website/Identity/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CornerstoneZearing.Website.Identity
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<ApplicationUser>(b => { b.ToTable("Users"); });
+            modelBuilder.Entity<IdentityRole>(b => { b.ToTable("Roles"); });
+            modelBuilder.Entity<IdentityUserRole<string>>(b => { b.ToTable("UserRoles"); });
+            modelBuilder.Entity<IdentityUserClaim<string>>(b => { b.ToTable("UserClaims"); });
+            modelBuilder.Entity<IdentityRoleClaim<string>>(b => { b.ToTable("RoleClaims"); });
+            modelBuilder.Entity<IdentityUserLogin<string>>(b => { b.ToTable("UserLogins"); });
+            modelBuilder.Entity<IdentityUserToken<string>>(b => { b.ToTable("UserTokens"); });
+        }
+    }
+}
