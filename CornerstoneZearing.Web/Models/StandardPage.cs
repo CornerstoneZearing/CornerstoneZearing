d3f5e798a27799f6f9063b4cd5792350c0b38a76
Ryan Heldt <ryan@ryanheldt.com>
2026-05-31 19:35:22 -0500

    Now using Piranha CMS

---
+++ CornerstoneZearing.Web/Models/StandardPage.cs
@@ -0,0 +1,10 @@
+using Piranha.AttributeBuilder;
+using Piranha.Models;
+
+namespace CornerstoneZearing.Web.Models
+{
+    [PageType(Title = "Standard page")]
+    public class StandardPage : Page<StandardPage>
+    {
+    }
+}
--- CornerstoneZearing.Website/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CornerstoneZearing.Website.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
