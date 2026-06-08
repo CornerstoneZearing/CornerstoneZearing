9707409a5b93b1b338689ac344b0c2f7cdfce46e
Ryan Heldt <ryan@ryanheldt.com>
2026-06-07 21:41:30 -0500

    Bundling and minification.

---
+++ CornerstoneZearing.Website/Packager/PackageTagHelper.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace CornerstoneZearing.Website.Packager
+{
+    [HtmlTargetElement("package", TagStructure = TagStructure.WithoutEndTag)]
+    public sealed class PackageTagHelper(PackageCollection packages, PackageProcessor processor) : TagHelper
+    {
+        [HtmlAttributeName("name")]
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Processes the package tag by retrieving the specified package and generating the appropriate HTML tags for its files.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="output"></param>
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = null;
+
+            var package = packages.Get(Name);
+            if (package is null)
+            {
+                return;
+            }
+
+            var result = processor.GetOrBuild(package);
+            var url = $"{package.VirtualPath}?v={result.Hash}";
+            var tag = package.Type == PackageType.Style ? $"""<link rel="stylesheet" href="{url}" />""" : $"""<script src="{url}"></script>""";
+            output.Content.SetHtmlContent(tag);
+        }
+    }
+}
--- CornerstoneZearing.Website/Program.cs
+++ CornerstoneZearing.Website/Program.cs
@@ -1,12 +1,30 @@
+using CornerstoneZearing.Website.Packager;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Add MVC and Cofoundry services
 builder.Services
     .AddMvc()
     .AddCofoundry(builder.Configuration);
 
-var app = builder.Build();
+// Add packages
+builder.Services.AddPackages(packages =>
+{
+    packages.Add(new StylePackage("~/styles/cornerstone.css")
+        .Include("~/styles/theme.css")
+        .Include("~/styles/theme-elements.css")
+        .Include("~/styles/theme-blog.css")
+        .Include("~/styles/theme-church.css")
+        .Include("~/styles/custom.css"));
 
+    packages.Add(new ScriptPackage("~/scripts/cornerstone.js")
+        .Include("~/scripts/app.js"));
+});
+
+// Start web application
+var app = builder.Build();
 app.UseHttpsRedirection();
 app.UseCofoundry();
-
+app.UseStaticFiles();
+app.UsePackages();
 app.Run();
