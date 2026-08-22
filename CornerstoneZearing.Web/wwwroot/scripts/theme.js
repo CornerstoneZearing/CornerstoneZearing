90d9ac777a76c9bd4e5e60ff52fc1aec44093098
Ryan Heldt <ryan@ryanheldt.com>
2026-08-22 12:33:20 -0500

    More work on theme

--- CornerstoneZearing.Web/wwwroot/scripts/theme.js
+++ CornerstoneZearing.Web/wwwroot/scripts/theme.js
@@ -1,3 +1,12 @@
 /* ---------------- */
 /* Theme JavaScript */
 /* ---------------- */
+
+const header = document.querySelector("header");
+
+const onScroll = () => {
+    header.classList.toggle("scrolled", window.scrollY > 0);
+};
+
+window.addEventListener("scroll", onScroll, { passive: true });
+onScroll();
